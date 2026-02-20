6cf1d590cc8bfd0004beb9b5bab5cff7dc541843
Vinicius dos Santos <viniciusreis.pro@gmail.com>
2026-02-20 12:43:00 -0300

    first commit

---
+++ src/main/java/io/getarrays/server/model/Server.java
@@ -0,0 +1,33 @@
+package io.getarrays.server.model;
+
+import io.getarrays.server.enumeration.Status;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import static jakarta.persistence.GenerationType.AUTO;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Server {
+    @Id
+    @GeneratedValue(strategy = AUTO)
+    private Long id;
+    @Column(unique = true)
+    @NotEmpty(message = "IP Adress cannot be empty or null")
+    private String ipAdress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
---
+++ src/main/java/io/getarrays/server/repo/ServerRepository.java
@@ -0,0 +1,8 @@
+package io.getarrays.server.repo;
+
+import io.getarrays.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
